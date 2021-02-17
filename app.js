b538a129b68067eebdd153b02e20c76ac4b7519d
MarkeyBass <MarkeyBass@gmail.com>
2021-02-18 00:02:51 +0200

    s bug

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const e = require('express');
 const express = require('express');
 const path = require('path');
 
@@ -7,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname ,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(5004, () => console.log('server is running on port 5004'));
