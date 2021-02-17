5f405ec66c4a5797964915e3ecd83c51f77ce18d
MarkeyBass <MarkeyBass@gmail.com>
2021-02-17 23:41:09 +0200

    my first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const e = require('express');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname ,'public')));
+
+app.listen(5004, () => console.log('server is running on port 5004'));
