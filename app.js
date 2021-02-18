c3360790768d93431dd9cc7315c4d1cea336022d
MarkeyBass <MarkeyBass@gmail.com>
2021-02-18 16:09:32 +0200

    https

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const express = require('express');
-const path = require('path');
+const http = require("http");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+const url = require('url');
 
+
+const express = require('express');
 const app = express();
+
+// REDIRECTION TO HTTPS
+
+const cert = fs.readFileSync('/etc/letsencrypt/archive/markeybass.com/cert1.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/archive/markeybass.com/chain1.pem', 'utf-8');
+const fullchain = fs.readFileSync('/etc/letsencrypt/archive/markeybass.com/fullchain1.pem', 'utf-8');
+const key = fs.readFileSync('/etc/letsencrypt/archive/markeybass.com/privkey1.pem', 'utf-8');
+
+const credentials = {
+  key,
+  cert,
+  ca
+} 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.listen(5004, () => console.log('server is running on port 5004'));
+const httpServer = http.createServer(app);
+const httpSecureServer = https.createServer(credentials ,app);
+
+httpPORT = 5004;
+httpSecurePORT = 8004;
+
+httpServer.listen(5004);
+httpSecureServer.listen(8004, () => console.log(`server is running on port ${httpSecurePORT}`));
